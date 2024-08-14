1d48ca59bbe1a9465ab168e1325b75c46ef738c2
wooni97 <jhw970714@gmail.com>
2024-08-14 15:45:22 +0900

    refactor: EmailAuthenticationRepositoryImpl 기본 반환값을 UnsupportedOperationException으로 변경

--- src/main/java/dev/flab/studytogether/domain/member/repository/EmailAuthenticationRepositoryImpl.java
+++ src/main/java/dev/flab/studytogether/domain/member/repository/EmailAuthenticationRepositoryImpl.java
@@ -3,7 +3,9 @@ package dev.flab.studytogether.domain.member.repository;
 import dev.flab.studytogether.domain.member.entity.EmailAuthentication;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+// TODO 추후 구현 예정
 @Repository
 public class EmailAuthenticationRepositoryImpl implements EmailAuthenticationRepository{
     @Override
@@ -13,11 +15,22 @@ public class EmailAuthenticationRepositoryImpl implements EmailAuthenticationRep
 
     @Override
     public Optional<EmailAuthentication> findByEmailAndAuthKey(String email, String authKey) {
-        return Optional.empty();
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<EmailAuthentication> findByEmail(String email) {
+        throw new UnsupportedOperationException();
+
+    }
+
+    @Override
+    public void delete(EmailAuthentication emailAuthentication) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public EmailAuthentication update(EmailAuthentication emailConfirm) {
-        return null;
+        throw new UnsupportedOperationException();
     }
 }
