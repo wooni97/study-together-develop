7d3b6a4332f8f0bfdd6046975a7420abd1378f9b
wooni97 <jhw970714@gmail.com>
2024-08-14 17:51:49 +0900

    delete: 사용하지 않는 클래스 삭제

--- src/main/java/dev/flab/studytogether/domain/member/exception/LinkExpiredException.java
+++
@@ -1,9 +0,0 @@
-package dev.flab.studytogether.domain.member.exception;
-
-import dev.flab.studytogether.exception.BadRequestException;
-
-public class LinkExpiredException extends BadRequestException {
-    public LinkExpiredException() {
-        super("링크가 만료되었습니다.");
-    }
-}
