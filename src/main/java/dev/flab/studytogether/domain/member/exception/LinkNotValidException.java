d988e0eba9354e400f2d95f00d484f53e562c6e1
wooni97 <jhw970714@gmail.com>
2024-08-14 17:56:43 +0900

    delete: 사용하지 않는 클래스 삭제

--- src/main/java/dev/flab/studytogether/domain/member/exception/LinkNotValidException.java
+++
@@ -1,9 +0,0 @@
-package dev.flab.studytogether.domain.member.exception;
-
-import dev.flab.studytogether.exception.BadRequestException;
-
-public class LinkNotValidException extends BadRequestException {
-    public LinkNotValidException() {
-        super("유효하지 않은 링크입니다.");
-    }
-}
