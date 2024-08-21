3071530ca2497aa06d9b20e992462416d658b21b
wooni97 <jhw970714@gmail.com>
2024-08-21 11:54:52 +0900

    chore: 어노테이션 추가

--- src/main/java/dev/flab/studytogether/domain/member/repository/MemberV2JpaRepository.java
+++ src/main/java/dev/flab/studytogether/domain/member/repository/MemberV2JpaRepository.java
@@ -2,8 +2,10 @@ package dev.flab.studytogether.domain.member.repository;
 
 import dev.flab.studytogether.domain.member.entity.MemberV2;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface MemberV2JpaRepository extends JpaRepository<MemberV2, Long> {
     boolean existsByEmail(String email);
     boolean existsByNickname(String nickName);
