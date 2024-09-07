26578e14d5228fa0d4b4f28ec950784a8a781bea
wooni97 <jhw970714@gmail.com>
2024-09-07 18:32:17 +0900

    feat: MemberV2SignUpEvent 클래스 추가
    - 회원가입시 발행되는 이벤트에 대한 정보를 담은 도메인

---
+++ src/main/java/dev/flab/studytogether/domain/member/event/MemberV2SignUpEvent.java
@@ -0,0 +1,21 @@
+package dev.flab.studytogether.domain.member.event;
+
+import lombok.Getter;
+
+@Getter
+public class MemberV2SignUpEvent {
+    private Long id;
+    private final String email;
+    private final String authKey;
+
+    public MemberV2SignUpEvent(Long id, String email, String authKey) {
+        this.id = id;
+        this.email = email;
+        this.authKey = authKey;
+    }
+
+    public MemberV2SignUpEvent(String email, String authKey) {
+        this.email = email;
+        this.authKey = authKey;
+    }
+}
