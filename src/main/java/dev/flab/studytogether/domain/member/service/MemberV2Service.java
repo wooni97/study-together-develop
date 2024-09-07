4f8537fcf604f871bc39b68e235042a787d6abd1
wooni97 <jhw970714@gmail.com>
2024-09-07 18:44:34 +0900

    refactor: MemberV2Service 회원가입 후 이메일 발행하도록 수정

--- src/main/java/dev/flab/studytogether/domain/member/service/MemberV2Service.java
+++ src/main/java/dev/flab/studytogether/domain/member/service/MemberV2Service.java
@@ -2,6 +2,7 @@ package dev.flab.studytogether.domain.member.service;
 
 import dev.flab.studytogether.domain.member.entity.EmailAuthentication;
 import dev.flab.studytogether.domain.member.entity.MemberV2;
+import dev.flab.studytogether.domain.member.event.MemberV2SignUpEvent;
 import dev.flab.studytogether.domain.member.exception.*;
 import dev.flab.studytogether.domain.member.repository.EmailAuthenticationRepository;
 import dev.flab.studytogether.domain.member.repository.MemberV2Repository;
@@ -16,8 +17,8 @@ import java.util.Optional;
 public class MemberV2Service {
     private final MemberV2Repository memberV2Repository;
     private final EmailAuthenticationRepository emailAuthenticationRepository;
-    private final NotificationService notificationService;
     private final PasswordEncoder passwordEncoder;
+    private final MemberV2SignUpEventPublishService memberV2SignUpEventPublishService;
 
 
     @Transactional
@@ -38,8 +39,10 @@ public class MemberV2Service {
         EmailAuthentication emailAuthentication = createEmailAuthentication(email);
 
         memberV2Repository.save(newMember);
+        emailAuthenticationRepository.save(emailAuthentication);
 
-        notificationService.sendEmailAddressVerification(email, emailAuthentication.getAuthKey());
+        memberV2SignUpEventPublishService.publishEvent(
+                new MemberV2SignUpEvent(newMember.getEmail(), emailAuthentication.getAuthKey()));
 
         return newMember;
     }
