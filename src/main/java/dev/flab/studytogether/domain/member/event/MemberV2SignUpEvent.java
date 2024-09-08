b165b944143a4befaed7a425351db5945a8b3074
wooni97 <jhw970714@gmail.com>
2024-09-08 15:49:42 +0900

    refactor: MemberV2SignUpEvent 클래스를 AbstractEvent 구현 클래스로 변경

--- src/main/java/dev/flab/studytogether/domain/member/event/MemberV2SignUpEvent.java
+++ src/main/java/dev/flab/studytogether/domain/member/event/MemberV2SignUpEvent.java
@@ -1,22 +1,25 @@
 package dev.flab.studytogether.domain.member.event;
 
+import dev.flab.studytogether.domain.event.AbstractEvent;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
-public class MemberV2SignUpEvent {
-    private final Long id;
-    private final String email;
+public class MemberV2SignUpEvent extends AbstractEvent {
+    private final Long userId;
+    private final String userEmail;
     private final String authKey;
 
-    public MemberV2SignUpEvent(Long id, String email, String authKey) {
-        this.id = id;
-        this.email = email;
+    private MemberV2SignUpEvent(Long userId, String userEmail, String authKey) {
+        super(LocalDateTime.now());
+        this.userId = userId;
+        this.userEmail = userEmail;
         this.authKey = authKey;
+
     }
 
-    public MemberV2SignUpEvent(String email, String authKey) {
-        this.id = null;
-        this.email = email;
-        this.authKey = authKey;
+    public static MemberV2SignUpEvent createEvent(Long userId, String userEmail, String authKey) {
+        return new MemberV2SignUpEvent(userId, userEmail, authKey);
     }
 }
