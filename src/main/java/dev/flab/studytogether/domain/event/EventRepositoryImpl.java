1e0cd2ccc021de12eee58a4fda9fd4645658f892
wooni97 <jhw970714@gmail.com>
2024-09-08 15:53:10 +0900

    feat: EventRepository 구현 클래스 추가

---
+++ src/main/java/dev/flab/studytogether/domain/event/EventRepositoryImpl.java
@@ -0,0 +1,43 @@
+package dev.flab.studytogether.domain.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class EventRepositoryImpl implements EventRepository {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void save(AbstractEvent event) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("EVENT").usingGeneratedKeyColumns("EVENT_ID");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String paylaod = null;
+        try {
+            paylaod = objectMapper.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            log.error("message : {}", e.getMessage());
+        }
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("EVENT_TYPE", event.getClass().getName());
+        parameters.put("PAYLOAD", paylaod);
+        parameters.put("CREATED_AT", event.getCreatedAt());
+
+        jdbcInsert.execute(parameters);
+    }
+}
