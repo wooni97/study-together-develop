a81d76c51a4ef79f1ee709897e3b66daa3027b31
wooni97 <jhw970714@gmail.com>
2024-09-10 13:13:24 +0900

    feat: 필요없는 클래스 삭제

--- src/main/java/dev/flab/studytogether/domain/event/EventPublishService.java
+++
@@ -1,7 +0,0 @@
-package dev.flab.studytogether.domain.event;
-
-import dev.flab.studytogether.domain.event.AbstractEvent;
-
-public interface EventPublishService {
-    void publish(AbstractEvent event);
-}
