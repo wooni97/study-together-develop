00b0600b9b6eec66d60d61855817800d2a2f9acf
wooni97 <jhw970714@gmail.com>
2024-09-24 03:35:02 +0900

    update: ParticipantV2와 StudyGroup 양방향 매핑 제거
    - 단방향으로 수정(ID 간접 참조)

--- src/main/java/dev/flab/studytogether/domain/studygroup/entity/ParticipantV2.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/entity/ParticipantV2.java
@@ -1,6 +1,5 @@
 package dev.flab.studytogether.domain.studygroup.entity;
 
-import dev.flab.studytogether.domain.member.entity.MemberV2;
 import dev.flab.studytogether.domain.room.entity.ParticipantRole;
 import lombok.Getter;
 
@@ -14,30 +13,25 @@ public class ParticipantV2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "STUDY_GROUP_ID")
-    private StudyGroup studyGroup;
-    @ManyToOne
-    @JoinColumn(name = "MEMBER_ID")
-    private MemberV2 memberV2;
+    private Long studyGroupId;
+    private Long memberId;
     @Enumerated(EnumType.STRING)
     private ParticipantRole participantRole;
     private LocalDateTime joinedAt;
 
-    private ParticipantV2(Long id, StudyGroup studyGroup, MemberV2 memberV2, ParticipantRole participantRole, LocalDateTime joinedAt) {
+    private ParticipantV2(Long id, Long studyGroupId, Long memberId, ParticipantRole participantRole, LocalDateTime joinedAt) {
         this.id = id;
-        this.studyGroup = studyGroup;
-        this.memberV2 = memberV2;
+        this.studyGroupId = studyGroupId;
+        this.memberId = memberId;
         this.participantRole = participantRole;
         this.joinedAt = joinedAt;
     }
 
-    private ParticipantV2(StudyGroup studyGroup, MemberV2 memberV2, ParticipantRole participantRole, LocalDateTime joinedAt) {
-        this.studyGroup = studyGroup;
-        this.memberV2 = memberV2;
+    private ParticipantV2(Long studyGroupId, Long memberId, ParticipantRole participantRole, LocalDateTime joinedAt) {
+        this.studyGroupId = studyGroupId;
+        this.memberId = memberId;
         this.participantRole = participantRole;
         this.joinedAt = joinedAt;
-        studyGroup.joinGroup(this);
     }
 
     protected ParticipantV2() {}
@@ -50,19 +44,19 @@ public class ParticipantV2 {
     public boolean equals(Object object) {
         if (this == object) return true;
         if (!(object instanceof ParticipantV2 that)) return false;
-        return Objects.equals(id, that.id) && Objects.equals(studyGroup, that.studyGroup) && Objects.equals(memberV2, that.memberV2) && participantRole == that.participantRole && Objects.equals(joinedAt, that.joinedAt);
+        return Objects.equals(id, that.id) && Objects.equals(studyGroupId, that.studyGroupId) && Objects.equals(memberId, that.memberId) && participantRole == that.participantRole && Objects.equals(joinedAt, that.joinedAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, studyGroup, memberV2, participantRole, joinedAt);
+        return Objects.hash(id, studyGroupId, memberId, participantRole, joinedAt);
     }
 
-    public static ParticipantV2 createExistingParticipant(Long id, StudyGroup studyGroup, MemberV2 memberV2, ParticipantRole participantRole, LocalDateTime joinedAt) {
-        return new ParticipantV2(id, studyGroup, memberV2, participantRole, joinedAt);
+    public static ParticipantV2 createExistingParticipant(Long id, Long studyGroupId, Long memberId, ParticipantRole participantRole, LocalDateTime joinedAt) {
+        return new ParticipantV2(id, studyGroupId, memberId, participantRole, joinedAt);
     }
 
-    public static ParticipantV2 createNewParticipant(StudyGroup studyGroup, MemberV2 member, ParticipantRole participantRole, LocalDateTime joinedAt) {
-        return new ParticipantV2(studyGroup, member, participantRole, joinedAt);
+    public static ParticipantV2 createNewParticipant(StudyGroup studyGroup, Long memberId, ParticipantRole participantRole, LocalDateTime joinedAt) {
+        return new ParticipantV2(studyGroup.getId(), memberId, participantRole, joinedAt);
     }
 }
