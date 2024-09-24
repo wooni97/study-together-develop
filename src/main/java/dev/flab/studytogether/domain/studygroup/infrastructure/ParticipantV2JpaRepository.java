81eeacac7e7043ffcb281522140d9eba18b1e7ab
wooni97 <jhw970714@gmail.com>
2024-09-24 18:18:56 +0900

    delete: ParticipantV2JpaRepository, ParticipantV2Repository 삭제
    - StudyGroup과 생명주기 동일하므로 불필요하여 삭제

--- src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/ParticipantV2JpaRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.flab.studytogether.domain.studygroup.infrastructure;
-
-
-import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipantV2JpaRepository extends JpaRepository<ParticipantV2, Long> {
-
-}
