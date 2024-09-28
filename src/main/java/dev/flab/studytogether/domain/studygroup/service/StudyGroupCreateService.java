bca6dc0e28c6f84216a109149d390edf6556d4c3
wooni97 <jhw970714@gmail.com>
2024-09-28 15:58:30 +0900

    feat: 스터디 그룹 생성 서비스 레이어 코드 추가

---
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupCreateService.java
@@ -0,0 +1,36 @@
+package dev.flab.studytogether.domain.studygroup.service;
+
+import dev.flab.studytogether.domain.room.entity.ActivateStatus;
+import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
+import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
+import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
+import dev.flab.studytogether.domain.studygroup.role.ParticipantRoleV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class StudyGroupCreateService {
+    private final StudyGroupRepository studyGroupRepository;
+
+    @Transactional
+    public StudyGroup createGroup(String groupTitle, int maxParticipants, Long groupCreatorMemberId) {
+        StudyGroup studyGroup = new StudyGroup(groupTitle,
+                maxParticipants,
+                ActivateStatus.ACTIVATED);
+
+        ParticipantV2 participant = new ParticipantV2(studyGroup,
+                groupCreatorMemberId,
+                ParticipantRoleV2.GROUP_MANAGER,
+                LocalDateTime.now());
+
+        studyGroup.joinGroup(participant);
+        studyGroupRepository.save(studyGroup);
+
+        return studyGroup;
+    }
+
+}
