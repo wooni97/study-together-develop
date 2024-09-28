30ac8732227bc77d221370984d523ab3ecfa8a77
wooni97 <jhw970714@gmail.com>
2024-09-28 15:58:43 +0900

    feat: 스터디 그룹 참여 서비스 레이어 코드 추가

---
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupJoinService.java
@@ -0,0 +1,34 @@
+package dev.flab.studytogether.domain.studygroup.service;
+
+import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
+import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
+import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
+import dev.flab.studytogether.domain.studygroup.role.ParticipantRoleV2;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
+@Service
+@AllArgsConstructor
+public class StudyGroupJoinService {
+    private final StudyGroupRepository studyGroupRepository;
+
+    @Transactional
+    public StudyGroup joinGroup(Long roomId, Long memberId) {
+        StudyGroup studyGroup = studyGroupRepository.findById(roomId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 ID의 Study Group입니다."));
+
+        ParticipantV2 participant = new ParticipantV2(studyGroup,
+                memberId,
+                ParticipantRoleV2.ORDINARY_PARTICIPANT,
+                LocalDateTime.now());
+
+        studyGroup.joinGroup(participant);
+        studyGroupRepository.save(studyGroup);
+
+        return studyGroup;
+    }
+}
