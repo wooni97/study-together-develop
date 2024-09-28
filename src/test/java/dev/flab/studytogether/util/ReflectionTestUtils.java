38177729c1699c9bc1cbf1dd9e5e11cbcdb0c7ef
wooni97 <jhw970714@gmail.com>
2024-09-28 16:00:23 +0900

    test: 테스트 유틸 클래스 추가
    - 리플렉션으로 필드에 값을 넣을 수 있는 메서드 구현
    - StudyGroup 인스턴스 생성 유틸리티 클래스 메서드 구현

---
+++ src/test/java/dev/flab/studytogether/util/ReflectionTestUtils.java
@@ -0,0 +1,15 @@
+package dev.flab.studytogether.util;
+
+import java.lang.reflect.Field;
+
+class ReflectionTestUtils {
+    static void setField(Object target, String fieldName, Object value) {
+        try {
+            Field field = target.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(target,value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/dev/flab/studytogether/util/TestFixtureUtils.java
+++ src/test/java/dev/flab/studytogether/util/TestFixtureUtils.java
@@ -1,6 +1,12 @@
 package dev.flab.studytogether.util;
 
 import dev.flab.studytogether.domain.member.entity.MemberV2;
+import dev.flab.studytogether.domain.room.entity.ActivateStatus;
+import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
+import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
+import dev.flab.studytogether.domain.studygroup.role.ParticipantRoleV2;
+
+import java.time.LocalDateTime;
 
 public class TestFixtureUtils {
     public static MemberV2 randomMember() {
@@ -9,5 +15,43 @@ public class TestFixtureUtils {
         String nickname = "testNickName";
 
         return MemberV2.createNewMember(email, password, nickname);
+    }
+
+    public static StudyGroup randomStudyGroup() {
+        String groupTitle = "Java Study Group";
+        int maxParticipants = 10;
+        ActivateStatus activateStatus = ActivateStatus.ACTIVATED;
+
+        return new StudyGroup(groupTitle, maxParticipants, activateStatus);
+    }
+
+    public static StudyGroup randomStudyGroupWithParticipants() {
+        String groupTitle = "Java Study Group";
+        int maxParticipants = 10;
+        ActivateStatus activateStatus = ActivateStatus.ACTIVATED;
+
+        StudyGroup studyGroup = new StudyGroup(groupTitle, maxParticipants, activateStatus);
+
+        ReflectionTestUtils.setField(studyGroup, "id", 1L);
+
+        ParticipantV2 manager = new ParticipantV2(studyGroup,
+                1L,
+                ParticipantRoleV2.GROUP_MANAGER,
+                LocalDateTime.of(2023, 1, 2, 4, 5));
+
+        ReflectionTestUtils.setField(manager, "id", 1L);
+
+        studyGroup.joinGroup(manager);
+
+        ParticipantV2 participant = new ParticipantV2(studyGroup,
+                2L,
+                ParticipantRoleV2.ORDINARY_PARTICIPANT,
+                LocalDateTime.of(2023, 2, 1, 2, 3, 55));
+
+        ReflectionTestUtils.setField(participant, "id", 2L);
+
+        studyGroup.joinGroup(participant);
+
+        return studyGroup;
     }
 }
