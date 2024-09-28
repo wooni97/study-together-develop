96ce3a92122d2fd89858b5caeddc8bcb0fbdde38
wooni97 <jhw970714@gmail.com>
2024-09-28 15:58:36 +0900

    feat: 스터디 그룹 퇴장 서비스 레이어 코드 추가

---
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupExitService.java
@@ -0,0 +1,24 @@
+package dev.flab.studytogether.domain.studygroup.service;
+
+import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
+import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+@AllArgsConstructor
+public class StudyGroupExitService {
+    private final StudyGroupRepository studyGroupRepository;
+
+    public StudyGroup exitGroup(Long roomId, Long participantId) {
+        StudyGroup studyGroup = studyGroupRepository.findById(roomId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 ID의 Study Group입니다."));
+
+        studyGroup.exitGroup(participantId);
+        studyGroupRepository.save(studyGroup);
+
+        return studyGroup;
+    }
+}
