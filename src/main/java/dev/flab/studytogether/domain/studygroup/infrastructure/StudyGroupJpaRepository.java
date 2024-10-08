ef4d8252f9ca53cf68639f02ed92653030e67f42
wooni97 <jhw970714@gmail.com>
2024-10-08 17:00:50 +0900

    feat: 비관적 락 어노테이션 추가

--- src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupJpaRepository.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupJpaRepository.java
@@ -4,12 +4,15 @@ import dev.flab.studytogether.domain.room.entity.ActivateStatus;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 public interface StudyGroupJpaRepository extends JpaRepository<StudyGroup, Long> {
     @EntityGraph(attributePaths = {"participants.participants"})
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<StudyGroup> findById(long id);
     List<StudyGroup> findByActivateStatus(ActivateStatus status);
 }
