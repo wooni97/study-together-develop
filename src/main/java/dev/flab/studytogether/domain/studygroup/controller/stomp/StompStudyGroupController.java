f59343a28412659a29541f249570284e65754594
wooni97 <jhw970714@gmail.com>
2024-11-05 12:10:42 +0900

    refactoring: StompStudyGroupController 코드 변경

--- src/main/java/dev/flab/studytogether/domain/studygroup/controller/stomp/StompStudyGroupController.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/controller/stomp/StompStudyGroupController.java
@@ -4,63 +4,39 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.flab.studytogether.domain.studygroup.service.StudyGroupExcessParticipantHandlerService;
-import dev.flab.studytogether.websocket.enums.StompMessageType;
+
 import dev.flab.studytogether.websocket.enums.PayloadType;
 import dev.flab.studytogether.websocket.ServerMessagePayload;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
-
-import java.security.Principal;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class StompStudyGroupController {
 
-    private final SimpMessagingTemplate messagingTemplate;
     private final StudyGroupExcessParticipantHandlerService excessParticipantHandlerService;
 
-    @MessageMapping("/study-group")
-    public void handleMessage(@Payload String message, Principal principal) throws JsonProcessingException {
+    @MessageMapping("/studyGroup/attempt/join")
+    @SendToUser("/queue/group")
+    public ServerMessagePayload attemptJoinStudyGroup(@Payload String message) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode messageNode = objectMapper.readTree(message);
-
-        StompMessageType messageType = StompMessageType.findByMessageType(messageNode.get("type").asText());
-
-        if(messageType.equals(StompMessageType.JOIN)) {
-            handleJoin(messageNode, principal);
-            return;
-        }
-
-        if(messageType.equals(StompMessageType.CHAT)) {
-            // 구현 예정
-            throw new UnsupportedOperationException();
-        }
+        return handleJoinAttemptAndReceiveMessage(messageNode);
     }
 
-    private void handleJoin(JsonNode messageNode, Principal principal) {
+    private ServerMessagePayload handleJoinAttemptAndReceiveMessage(JsonNode messageNode) {
         Long groupId = messageNode.get("groupId").asLong();
+        Long participantId = messageNode.get("participantId").asLong();
 
-        List<Long> latestParticipantIds =
-                excessParticipantHandlerService.handleExcessParticipants(groupId);
+        boolean isDeactivated = excessParticipantHandlerService
+                        .isParticipantDeactivatedIfOverCapacity(groupId, participantId);
 
-        if(latestParticipantIds.contains(messageNode.get("participantId").asLong())) {
-            messagingTemplate.convertAndSendToUser(
-                    principal.getName(),
-                    "/queue/group" + groupId,
-                    new ServerMessagePayload(PayloadType.JOIN_LIMIT_REACHED, "정원 초과로 입장 실패")
-                    );
-
-            return;
-        }
-
-        messagingTemplate.convertAndSendToUser(
-                principal.getName(),
-                "/queue/group" + groupId,
-                new ServerMessagePayload(PayloadType.JOIN_SUCCESS, "입장 완료")
-        );
+        return isDeactivated
+                ? new ServerMessagePayload(PayloadType.JOIN_LIMIT_REACHED, "정원 초과로 입장 실패")
+                : new ServerMessagePayload(PayloadType.JOIN_SUCCESS, "입장 완료");
     }
 }
