c2af50e784ca210a191c6248b42ff17b25755c09
wooni97 <jhw970714@gmail.com>
2024-11-05 12:11:02 +0900

    config: STOMP 웹소켓 설정 정보 변경

--- src/main/java/dev/flab/studytogether/config/StompWebSocketConfig.java
+++ src/main/java/dev/flab/studytogether/config/StompWebSocketConfig.java
@@ -1,6 +1,5 @@
 package dev.flab.studytogether.config;
 
-import dev.flab.studytogether.websocket.CustomHandShakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -12,23 +11,19 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer{
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        // "stomp-room" 는 웹소캣 또는 SocketJS Client가 웹소캣 핸드셰이크 커넥션을 생성할 경로이다.
         registry.addEndpoint("/stomp-room")
                 .setAllowedOrigins("http://localhost:8080")
                 .withSockJS();
 
-        registry.addEndpoint("/study-group")
+        registry.addEndpoint("/studyGroup")
                 .setAllowedOrigins("*")
-                .setHandshakeHandler(new CustomHandShakeHandler())
                 .withSockJS();
-
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-           config.enableSimpleBroker("/subscribe");
+           config.enableSimpleBroker("/topic", "/queue", "/subscribe");
            config.setApplicationDestinationPrefixes("/publish");
     }
-
 
 }
