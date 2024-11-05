8e66236b9a73d1643dccb622ae7a1dd5650950ec
wooni97 <jhw970714@gmail.com>
2024-11-05 12:12:50 +0900

    chore: 불필요한 import문 삭제

--- src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupJpaRepository.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupJpaRepository.java
@@ -4,9 +4,7 @@ import dev.flab.studytogether.domain.room.entity.ActivateStatus;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 
-import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
