2c7f25fa13b5e4205152af2bbe4cc472a3e6cefb
wooni97 <jhw970714@gmail.com>
2024-11-08 00:20:35 +0900

    feat: Study Group 입장시 이벤트 생성하여 아웃박스 테이블에 저장하도록 코드 추가
    - StudyGroupJoinService 클래스에서 스터디 그룹 입장 시, WAITING 상태로 입장 후 이벤트 생성 및 저장

--- src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupJoinService.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupJoinService.java
@@ -1,8 +1,10 @@
 package dev.flab.studytogether.domain.studygroup.service;
 
+import dev.flab.studytogether.domain.event.EventRepository;
 import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
-import dev.flab.studytogether.domain.studygroup.exception.MemberNotFoundInGroupException;
+import dev.flab.studytogether.domain.studygroup.event.StudyGroupJoinEvent;
+import dev.flab.studytogether.domain.studygroup.exception.ParticipantWithMemberIdNotFoundInGroupException;
 import dev.flab.studytogether.domain.studygroup.exception.StudyGroupNotFoundException;
 import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
 import lombok.AllArgsConstructor;
@@ -16,24 +18,30 @@ import java.util.Objects;
 @AllArgsConstructor
 public class StudyGroupJoinService {
     private final StudyGroupRepository studyGroupRepository;
+    private final EventRepository eventRepository;
 
     @Transactional
     public ParticipantV2 joinGroup(Long roomId, Long memberId) {
         StudyGroup studyGroup = studyGroupRepository.findById(roomId)
                 .orElseThrow(() -> new StudyGroupNotFoundException(roomId));
 
-        ParticipantV2 participant = new ParticipantV2(studyGroup,
+        studyGroup.joinGroup(new ParticipantV2(studyGroup,
                 memberId,
                 ParticipantV2.Role.ORDINARY_PARTICIPANT,
-                LocalDateTime.now());
-
-        studyGroup.joinGroup(participant);
+                ParticipantV2.ParticipantStatus.WAITING,
+                LocalDateTime.now()));
         studyGroupRepository.save(studyGroup);
 
-        return studyGroup.getParticipants().getParticipants()
+        ParticipantV2 joinedParticipant = studyGroup.getParticipants().getParticipants()
                 .stream()
                 .filter(p -> Objects.equals(p.getMemberId(), memberId))
                 .findFirst()
-                .orElseThrow(() -> new MemberNotFoundInGroupException(roomId, memberId));
+                .orElseThrow(() -> new ParticipantWithMemberIdNotFoundInGroupException(roomId, memberId));
+
+        StudyGroupJoinEvent studyGroupJoinEvent =
+                StudyGroupJoinEvent.createNewEvent(studyGroup.getId(), joinedParticipant.getId());
+        eventRepository.save(studyGroupJoinEvent);
+
+        return joinedParticipant;
     }
 }
