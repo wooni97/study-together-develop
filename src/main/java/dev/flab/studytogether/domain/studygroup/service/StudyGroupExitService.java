627313f639142d057266608daf7dc232ad46bd29
wooni97 <jhw970714@gmail.com>
2024-11-08 00:30:05 +0900

    refactor: StudyGroupExitService 클래스 리팩토링
    - 해당 서비스 클래스가 이벤트 처리 과정에서 사용되는 것으로 변경됨에 따라 parameter 수정

--- src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupExitService.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupExitService.java
@@ -1,25 +1,27 @@
 package dev.flab.studytogether.domain.studygroup.service;
 
+import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import dev.flab.studytogether.domain.studygroup.exception.StudyGroupNotFoundException;
 import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.NoSuchElementException;
-
-@Service
+@Service 
 @AllArgsConstructor
 public class StudyGroupExitService {
+    
     private final StudyGroupRepository studyGroupRepository;
+    
+    public void exitGroup(Long studyGroupId, Long participantId) {
+        StudyGroup studyGroup = studyGroupRepository.findById(studyGroupId)
+                .orElseThrow(() ->
+                        new StudyGroupNotFoundException(studyGroupId));
 
-    public StudyGroup exitGroup(Long roomId, Long memberId, Long participantId) {
-        StudyGroup studyGroup = studyGroupRepository.findById(roomId)
-                .orElseThrow(() -> new StudyGroupNotFoundException(roomId));
+        ParticipantV2 participant = studyGroup.getParticipantByParticipantId(participantId);
 
-        studyGroup.exitGroup(memberId, participantId);
+        studyGroup.exitGroup(participant.getMemberId(), participant.getId());
+
         studyGroupRepository.save(studyGroup);
-
-        return studyGroup;
     }
 }
