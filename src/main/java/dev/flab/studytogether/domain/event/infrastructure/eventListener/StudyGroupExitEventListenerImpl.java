ae3d1bf0c0e35c41f6e4f440e4ae44f26cdd716c
wooni97 <jhw970714@gmail.com>
2024-11-08 00:36:33 +0900

    feat: EventListener 구현체들 추가

---
+++ src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupExitEventListenerImpl.java
@@ -0,0 +1,22 @@
+package dev.flab.studytogether.domain.event.infrastructure.eventListener;
+
+import dev.flab.studytogether.domain.event.EventListener;
+import dev.flab.studytogether.domain.studygroup.event.StudyGroupExitEvent;
+import dev.flab.studytogether.domain.studygroup.service.StudyGroupExitService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class StudyGroupExitEventListenerImpl implements EventListener<StudyGroupExitEvent> {
+
+    private final StudyGroupExitService studyGroupExitService;
+
+    @Override
+    @org.springframework.context.event.EventListener(StudyGroupExitEvent.class)
+    public void handleEvent(StudyGroupExitEvent event) {
+        studyGroupExitService.exitGroup(
+                event.getStudyGroupId(),
+                event.getParticipantId());
+    }
+}
