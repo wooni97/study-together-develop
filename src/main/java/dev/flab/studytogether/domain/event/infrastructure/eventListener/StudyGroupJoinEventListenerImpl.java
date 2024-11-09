c3f83903d56d04d3353bd88c8e00ac9a424ef957
wooni97 <jhw970714@gmail.com>
2024-11-09 12:33:56 +0900

    fix: StudyGroupJoinEventListenerImpl 클래스 수정
    - 기존 코드 문제 상황 : Participant 클래스에서는 where 절을 통해 현재 입장한 인원만을 가져오기에, 리스너 클래스의 메서드에서 아직 입장 완료 전인 Participant를 찾을 수 없는 문제
    - 수정 : where 조건 절 제거 및 입장과 관련한 로직은 별도의 서비스 클래스 로직에 위임

--- src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupJoinEventListenerImpl.java
+++ src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupJoinEventListenerImpl.java
@@ -1,28 +1,24 @@
 package dev.flab.studytogether.domain.event.infrastructure.eventListener;
 
 import dev.flab.studytogether.domain.event.EventListener;
-import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
-import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import dev.flab.studytogether.domain.studygroup.event.StudyGroupJoinEvent;
-import dev.flab.studytogether.domain.studygroup.exception.StudyGroupNotFoundException;
-import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
+import dev.flab.studytogether.domain.studygroup.service.StudyGroupJoinService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class StudyGroupJoinEventListenerImpl implements EventListener<StudyGroupJoinEvent> {
 
-    private final StudyGroupRepository studyGroupRepository;
+    private final StudyGroupJoinService studyGroupJoinService;
+
     @Override
     @org.springframework.context.event.EventListener(StudyGroupJoinEvent.class)
     public void handleEvent(StudyGroupJoinEvent event) {
-        StudyGroup studyGroup = studyGroupRepository.findById(event.getStudyGroupId())
-                .orElseThrow(() -> new StudyGroupNotFoundException(event.getStudyGroupId()));
-
-        ParticipantV2 participantV2 = studyGroup.getParticipantByParticipantId(event.getParticipantId());
-        participantV2.changeParticipatingStatus(ParticipantV2.ParticipantStatus.JOINED);
-
-        studyGroupRepository.save(studyGroup);
+        studyGroupJoinService.joinGroup(
+                event.getStudyGroupId(),
+                event.getMemberId());
     }
 }
