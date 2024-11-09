bac25939f44fbeeb34e877d9b7f052d4ee7c9571
wooni97 <jhw970714@gmail.com>
2024-11-09 12:25:42 +0900

    refactor: StudyGroupExitProcessStartService 클래스 수정

--- src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupExitProcessStartService.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/service/StudyGroupExitProcessStartService.java
@@ -1,9 +1,9 @@
 package dev.flab.studytogether.domain.studygroup.service;
 
 import dev.flab.studytogether.domain.event.EventRepository;
-import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import dev.flab.studytogether.domain.studygroup.event.StudyGroupExitEvent;
+import dev.flab.studytogether.domain.studygroup.exception.ParticipantWithMemberIdNotFoundInGroupException;
 import dev.flab.studytogether.domain.studygroup.exception.StudyGroupNotFoundException;
 import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
 import lombok.AllArgsConstructor;
@@ -22,13 +22,13 @@ public class StudyGroupExitProcessStartService {
         StudyGroup studyGroup = studyGroupRepository.findById(roomId)
                 .orElseThrow(() -> new StudyGroupNotFoundException(roomId));
 
-        ParticipantV2 exitingParticipant = studyGroup.getParticipantByMemberId(memberId);
-        exitingParticipant.changeParticipatingStatus(
-                ParticipantV2.ParticipantStatus.EXIT_WAITING);
+        if(!studyGroup.isMemberJoined(memberId)) {
+            throw new ParticipantWithMemberIdNotFoundInGroupException(studyGroup.getId(), memberId);
+        }
+
         StudyGroupExitEvent studyGroupExitEvent =
                 StudyGroupExitEvent.createNewEvent(studyGroup.getId(), participantId);
 
-        studyGroupRepository.save(studyGroup);
         eventRepository.save(studyGroupExitEvent);
 
         return studyGroup;
