3240124c0f0c422978e18add6f38586f5c692606
wooni97 <jhw970714@gmail.com>
2024-11-11 23:17:21 +0900

    refactor: Repository 레이어 불필요한 메서드 삭제

--- src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupRepositoryImpl.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/infrastructure/StudyGroupRepositoryImpl.java
@@ -1,16 +1,11 @@
 package dev.flab.studytogether.domain.studygroup.infrastructure;
 
-import dev.flab.studytogether.domain.room.entity.ActivateStatus;
-import dev.flab.studytogether.domain.studygroup.entity.ParticipantV2;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 import dev.flab.studytogether.domain.studygroup.repository.StudyGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -28,26 +23,5 @@ public class StudyGroupRepositoryImpl implements StudyGroupRepository {
     @Override
     public Optional<StudyGroup> findById(long id) {
         return studyGroupJpaRepository.findById(id);
-    }
-
-    @Override
-    public List<StudyGroup> findByActivateStatus(ActivateStatus status) {
-        return studyGroupJpaRepository.findByActivateStatus(status);
-    }
-
-    @Override
-    public List<Long> findParticipantIdsWithinLimit(StudyGroup studyGroup) {
-        String query = "SELECT id FROM participantv2 " +
-                "WHERE study_group_id = ? " +
-                "and is_participating = true " +
-                "ORDER BY joined_at ASC " +
-                "LIMIT ?";
-
-        return jdbcTemplate.queryForList(
-                query,
-                Long.TYPE,
-                studyGroup.getId(),
-                studyGroup.getMaxParticipants()
-        );
     }
 }
--- src/main/java/dev/flab/studytogether/domain/studygroup/repository/StudyGroupRepository.java
+++ src/main/java/dev/flab/studytogether/domain/studygroup/repository/StudyGroupRepository.java
@@ -1,14 +1,10 @@
 package dev.flab.studytogether.domain.studygroup.repository;
 
-import dev.flab.studytogether.domain.room.entity.ActivateStatus;
 import dev.flab.studytogether.domain.studygroup.entity.StudyGroup;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface StudyGroupRepository {
     StudyGroup save(StudyGroup studyGroup);
     Optional<StudyGroup> findById(long id);
-    List<StudyGroup> findByActivateStatus(ActivateStatus status);
-    List<Long> findParticipantIdsWithinLimit(StudyGroup studyGroup);
 }
