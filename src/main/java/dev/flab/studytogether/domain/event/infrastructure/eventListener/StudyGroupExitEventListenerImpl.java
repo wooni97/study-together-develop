371a7128e765f6672d8d198865fd276f07c5a620
wooni97 <jhw970714@gmail.com>
2024-11-11 17:26:09 +0900

    fix: EventListener에서의 예외가 Consumer까지 전파되지 않도록 수정

--- src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupExitEventListenerImpl.java
+++ src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupExitEventListenerImpl.java
@@ -2,8 +2,11 @@ package dev.flab.studytogether.domain.event.infrastructure.eventListener;
 
 import dev.flab.studytogether.domain.event.EventListener;
 import dev.flab.studytogether.domain.studygroup.event.StudyGroupExitEvent;
+import dev.flab.studytogether.domain.studygroup.exception.NoParticipantForManagerDelegateException;
 import dev.flab.studytogether.domain.studygroup.service.StudyGroupExitService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class StudyGroupExitEventListenerImpl implements EventListener<StudyGroupExitEvent> {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final StudyGroupExitService studyGroupExitService;
 
     @Override
     @org.springframework.context.event.EventListener(StudyGroupExitEvent.class)
     public void handleEvent(StudyGroupExitEvent event) {
-        studyGroupExitService.exitGroup(
-                event.getStudyGroupId(),
-                event.getParticipantId());
+        try {
+            studyGroupExitService.exitGroup(
+                    event.getStudyGroupId(),
+                    event.getParticipantId());
+        } catch (NoParticipantForManagerDelegateException e) {
+            log.error("error : {} , message : {}", e.getClass(), e.getMessage());
+        }
+
     }
 }
--- src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupJoinEventListenerImpl.java
+++ src/main/java/dev/flab/studytogether/domain/event/infrastructure/eventListener/StudyGroupJoinEventListenerImpl.java
@@ -2,8 +2,12 @@ package dev.flab.studytogether.domain.event.infrastructure.eventListener;
 
 import dev.flab.studytogether.domain.event.EventListener;
 import dev.flab.studytogether.domain.studygroup.event.StudyGroupJoinEvent;
+import dev.flab.studytogether.domain.studygroup.exception.GroupCapacityExceededException;
+import dev.flab.studytogether.domain.studygroup.exception.TerminatedGroupJoinException;
 import dev.flab.studytogether.domain.studygroup.service.StudyGroupJoinService;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +16,18 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class StudyGroupJoinEventListenerImpl implements EventListener<StudyGroupJoinEvent> {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final StudyGroupJoinService studyGroupJoinService;
 
     @Override
     @org.springframework.context.event.EventListener(StudyGroupJoinEvent.class)
     public void handleEvent(StudyGroupJoinEvent event) {
-        studyGroupJoinService.joinGroup(
-                event.getStudyGroupId(),
-                event.getMemberId());
+        try {
+            studyGroupJoinService.joinGroup(
+                    event.getStudyGroupId(),
+                    event.getMemberId());
+        } catch(GroupCapacityExceededException | TerminatedGroupJoinException e) {
+            log.error("error : {} message : {}", e.getClass(), e.getMessage());
+        }
     }
 }
