56db7d7ee05db5608fc1cff31c30cd4ee8bbfb7e
wooni97 <jhw970714@gmail.com>
2024-11-11 23:11:57 +0900

    delete: 불필요한 클래스 파일 삭제

--- src/main/java/dev/flab/studytogether/websocket/ServerMessagePayload.java
+++
@@ -1,7 +0,0 @@
-package dev.flab.studytogether.websocket;
-
-
-import dev.flab.studytogether.websocket.enums.PayloadType;
-
-public record ServerMessagePayload(PayloadType payloadType, Object data) {
-}
--- src/main/java/dev/flab/studytogether/websocket/enums/PayloadType.java
+++
@@ -1,7 +0,0 @@
-package dev.flab.studytogether.websocket.enums;
-
-public enum PayloadType {
-    JOIN_SUCCESS,
-    JOIN_LIMIT_REACHED,
-    CHAT
-}
