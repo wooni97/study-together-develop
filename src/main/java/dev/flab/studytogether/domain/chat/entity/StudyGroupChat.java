eb75db120383e9254a7d654616577cdbd8496551
wooni97 <jhw970714@gmail.com>
2024-11-15 18:14:01 +0900

    chore: @DiscriminatorValue 추가

--- src/main/java/dev/flab/studytogether/domain/chat/entity/StudyGroupChat.java
+++ src/main/java/dev/flab/studytogether/domain/chat/entity/StudyGroupChat.java
@@ -2,11 +2,13 @@ package dev.flab.studytogether.domain.chat.entity;
 
 import lombok.Getter;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@DiscriminatorValue("STUDY_GROUP_CHAT")
 public class StudyGroupChat extends Chat {
     private Long studyGroupId;
 
