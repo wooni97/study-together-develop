4266a95871e96bc6d1be3f9d1699c70fa0688cf5
wooni97 <jhw970714@gmail.com>
2024-11-26 14:41:31 +0900

    chore: 변수명 변경

--- src/main/java/dev/flab/studytogether/infra/repository/chat/StudyGroupChatJpaRepository.java
+++ src/main/java/dev/flab/studytogether/infra/repository/chat/StudyGroupChatJpaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface StudyGroupChatJpaRepository extends JpaRepository<StudyGroupChat, Long> {
-    Optional<StudyGroupChat> findByStudyGroupId(Long StudyGroupId);
+    Optional<StudyGroupChat> findByStudyGroupId(Long studyGroupId);
 }
