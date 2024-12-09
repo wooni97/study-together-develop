8a5274d781bed176e678851f71d425fe3f4eb5c7
wooni97 <jhw970714@gmail.com>
2024-12-09 19:08:15 +0900

    refactor: AbstractNotificationData 필드 수정
    - 알림을 받는 멤버 아이디라는 의미를 명확히 나타내고자 변경
    - 정보를 추상화한 클래스에 scheduledAt은 구체적 맥락이 들어간것이므로 삭제

--- src/main/java/dev/flab/studytogether/core/domain/notification/AbstractNotificationData.java
+++ src/main/java/dev/flab/studytogether/core/domain/notification/AbstractNotificationData.java
@@ -3,17 +3,11 @@ package dev.flab.studytogether.core.domain.notification;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Getter
 @RequiredArgsConstructor
 public abstract class AbstractNotificationData {
 
     private final String notificationMessageContent;
-    private final Long memberId;
-    private final LocalDateTime scheduledAt;
-
-    public boolean isScheduled() {
-        return scheduledAt != null && scheduledAt.isAfter(LocalDateTime.now());
-    }
+    private final Long notifiedMemberId;
 }
